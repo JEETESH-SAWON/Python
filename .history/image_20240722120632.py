0c730b309e0d53a200d8e6cfdb4648a4242b662c
JEETESH SAWON <80146097+JEETESH-SAWON@users.noreply.github.com>
2024-07-23 07:20:58 +0530

    Delete .history directory

--- .history/image_20240722120632.py
+++
@@ -1,3 +0,0 @@
-from PIL import Image
-# input image from Directory
-iput_image = Image.open("/image/test.jpg")
--- .history/image_20240722120737.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-# input image from Directory
-input_image = Image.open("/image/test.jpg")
-input_image.show()
